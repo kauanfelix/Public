fd4604d74ed619580ed773f257266f4daf95ffc3
kauanfelix <85039573+kauanfelix@users.noreply.github.com>
2022-10-30 18:25:05 -0300

    18:25

--- home.py
+++ home.py
@@ -33,3 +33,6 @@ with tab1:
 with tab3:
     df = pd.read_excel(io='C:/Users/admin/PycharmProjects/pythonProject/testes.xlsx',engine='openpyxl',sheet_name="MOTORISTAS")
     st.dataframe(df)
+
+with tab3:
+    st.write("BACK MARKET: [BACKMARKET](https://www.backmarket.com/en-us)")
