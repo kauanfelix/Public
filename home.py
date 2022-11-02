8fd4729fb8ae0aeca13f6941b566c4db5abb393e
kauanfelix <85039573+kauanfelix@users.noreply.github.com>
2022-11-02 19:48:47 -0300

    Update home.py

--- home.py
+++ home.py
@@ -1,31 +1,36 @@
 import streamlit as st
-import pandas
 from streamlit_option_menu import option_menu
 import requests
-import json
+import pandas as pd
 
-
-st.set_page_config(page_title="Importações", page_icon=":laptop:", layout="centered")
+st.set_page_config(page_title="Gestão", page_icon=":bar-chart:", layout="centered")
 # with st.sidebar:
 #    selected = option_menu(menu_title=None, ["Fornecedores", 'Simulador', 'Importações', 'Gráficos'], icons=['credit-card', 'cash', 'cart', 'bar-chart'], menu_icon="cast", default_index=1)
-selected = option_menu(None, ['Fornecedores', 'Simulador', 'Importações', 'Gráficos'],
-                       icons=['credit-card', 'cash', 'cart', 'bar-chart'], orientation='horizontal', menu_icon="cast",
+selected = option_menu(None, [ 'Financeiro', 'Importações'],
+                       icons=['cash', 'cart'], orientation='horizontal', menu_icon="cast",
                        default_index=1)
 
-if selected == 'Fornecedores':
+if selected == 'Financeiro':
     st.title(selected)
+    df = pd.read_excel(io='debitos.xlsx', engine='openpyxl', sheet_name='DEBITOS', skiprows=0, usecols='A:C',nrows=100)
+
+
+if selected == 'Importações':
+    st.title(selected)
+    st.markdown('---')
+    st.subheader('Fornecedores')
+    requisicao = requests.get('https://economia.awesomeapi.com.br/all/USD-BRL')
+    cotacao = requisicao.json()
+    st.sidebar.text('DOLAR HOJE')
+    dolar = st.sidebar.write(cotacao['USD']['bid'])
     st.write("BACKMARKET: [RECONDICIONADOS](https://www.backmarket.com/en-us)")
     st.write("COMPRAS PARAGUAI: [NOVO](https://mobile.comprasparaguai.com.br/)")
     st.write("APPLE: [NOVO](https://www.apple.com/)")
-
-if selected == 'Simulador':
-    st.title(selected)
-    requisicao = requests.get('https://economia.awesomeapi.com.br/all/USD-BRL')
-    cotacao = requisicao.json()
-    dolar = st.write(cotacao['USD']['bid'])
-    col1, col2 =st.columns(2)
+    st.markdown('---')
+    st.subheader('Simular')
+    col1, col2 = st.columns(2)
     with col1:
-        preco = st.number_input('Preço:', 0, 10000, 50, step=100)
+        preco = st.number_input('Preço:', 0, 10000, 1000, step=100)
         imposto = st.number_input('Imposto:', 0, 60, 7)
         taxaloja = st.number_input('Taxa Loja:', 0.00, 4.99, 2.99, 1.00)
         resultado = ((imposto / 100) * preco + preco)
@@ -38,7 +43,10 @@ if selected == 'Simulador':
         iof = (spread * (input_iof / 100)) + spread
         total = st.info(f'R$ {iof * resultado}')
         st.write(iof)
-if selected == 'Importações':
-    st.title(selected)
-    st.write(
-        "GOOGLE PLANILHA: [REGISTROS](https://docs.google.com/spreadsheets/d/1PwVx36z6Anr2PpsTLKt24wBGuCaT4GShj3aUdoa9u4I/edit#gid=0)")
+    st.markdown('---')
+    st.subheader('Registros')
+    st.write("GOOGLE PLANILHA: [REGISTROS](https://docs.google.com/spreadsheets/d/1PwVx36z6Anr2PpsTLKt24wBGuCaT4GShj3aUdoa9u4I/edit#gid=0)")
+    st.markdown('---')
+
+
+
