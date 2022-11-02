f2d94c22dee81b409cce2c714321b48fa6bb2d0d
kauanfelix <85039573+kauanfelix@users.noreply.github.com>
2022-11-01 23:32:08 -0300

    23:32

--- home.py
+++ home.py
@@ -45,7 +45,22 @@ import streamlit as st
 from streamlit_option_menu import option_menu
 
 with st.sidebar:
-    selected = option_menu("Main Menu", ["Home", 'Settings'],
-        icons=['house', 'gear'], menu_icon="cast", default_index=1)
+    selected = option_menu("Menu", ["Fornecedores", 'Importações','Simulador'],
+        icons=['credit-card', 'pc','cash'], menu_icon="cast", default_index=1)
     selected
+    
+if selected == 'Simulador':
+    st.title('Simulador importação')
+    preco = st.number_input('Preço:', 1, 999, step=1)
+    imposto = st.number_input('Imposto:', 0, 60, 7)
+    taxaloja = st.number_input('Taxa Loja:', 0.00, 4.99, 2.99, 1.00)
+    resultado = ((imposto / 100) * preco + preco)
+    input_cotacao = st.number_input('Câmbio Dólar:', 4.50, 5.50, 5.00, 0.01)
+    input_spread = st.number_input('Spread:', 0.0, 5.0, 1.0)
+    input_iof = st.number_input('Iof:', 1.0, 6.39, 1.1, 5.28)
+    spread = (input_cotacao * (input_spread / 100)) + input_cotacao
+    iof = (spread * (input_iof / 100)) + spread
+    st.write(iof)
+    st.info(f'U$ {resultado + taxaloja}')
+    total = st.info(f'R$ {iof * resultado}')
 
