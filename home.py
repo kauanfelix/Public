bde21cc43327aa7d96fa09a7416f30baa42c948f
kauanfelix <85039573+kauanfelix@users.noreply.github.com>
2022-11-04 01:52:06 -0300

    Update home.py

--- home.py
+++ home.py
@@ -2,7 +2,7 @@ import streamlit as st
 from streamlit_option_menu import option_menu
 import requests
 import pandas as pd
-import plotly-express as px
+import plotly.express as px
 
 st.set_page_config(page_title="GESTÃO PESSOAL", page_icon=":bar-chart:", layout="centered")
 # with st.sidebar:
