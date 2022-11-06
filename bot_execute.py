8c68ae96eba609b3d92ce2861f0fe6dc409e4f49
kauanfelix <85039573+kauanfelix@users.noreply.github.com>
2022-11-06 17:32:28 -0300

    17:32

--- bot_execute.py
+++ bot_execute.py
@@ -1,7 +1,8 @@
+# pip install pyinstaller
+# pyinstaller -w C:/Users/admin/OneDrive/Nuvem-Kauan/bot_execute.py
 import pyautogui
 import time
 pyautogui.hotkey('ctrl', 'alt', 'z')
 time.sleep(5)
 pyautogui.write('streamlit run C:/Users/admin/PycharmProjects/pythonProject/webapp.py')
 pyautogui.press('Enter')
-
