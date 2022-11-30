9ca2f9a949464042cbddd86cd971cf0fa3672ac5
kauanfelix <85039573+kauanfelix@users.noreply.github.com>
2022-11-30 11:06:44 -0300

    Update form.py

--- form.py
+++ form.py
@@ -10,28 +10,24 @@ with st.form("my_form"):
    # Every form must have a submit button.
    submitted = st.form_submit_button("Submit")
    if submitted:
-       st.write("slider", slider_val, "checkbox", checkbox_val)
-
-
-
-        def enviar_email(): 
-            corpo_email = '''
+        st.write("slider", slider_val, "checkbox", checkbox_val)        
+        corpo_email = '''
             <p>Parágrafo1</p>
             <p>Parágrafo2</p>
             '''
 
-            msg = email.message.Message()
-            msg['Subject'] = 'Assunto'
-            msg['From'] = 'kauanfelix@icloud.com'
-            msg['To'] = 'administrativo@rodobrotto.com.br'
-            password = 'kvvlmclppzxufuys'
-            msg.add_header('Content-Type', 'text/html')
-            msg.set_payload(corpo_email )
+        msg = email.message.Message()
+        msg['Subject'] = 'Assunto'
+        msg['From'] = 'kauanfelix@icloud.com'
+        msg['To'] = 'administrativo@rodobrotto.com.br'
+        password = 'kvvlmclppzxufuys'
+        msg.add_header('Content-Type', 'text/html')
+        msg.set_payload(corpo_email )
 
-            s = smtplib.SMTP('smtp.gmail.com: 587')
-            s.starttls()
+        s = smtplib.SMTP('smtp.gmail.com: 587')
+        s.starttls()
             # Login Credentials for sending the mail
-            s.login(msg['From'], password)
-            s.sendmail(msg['From'], [msg['To']], msg.as_string().encode('utf-8'))
-            print('Email enviado')
+        s.login(msg['From'], password)
+        s.sendmail(msg['From'], [msg['To']], msg.as_string().encode('utf-8'))
+        print('Email enviado')
 st.write("Outside the form")
