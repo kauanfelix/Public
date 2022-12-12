097b2b00a3ee4eb4405671c10ff226a206268aee
kauanfelix <85039573+kauanfelix@users.noreply.github.com>
2022-12-12 14:51:35 -0300

    Update Logistic.py

--- Logistic.py
+++ Logistic.py
@@ -2,6 +2,7 @@ import pandas as pd
 import streamlit as st
 from st_aggrid import AgGrid, GridOptionsBuilder  # pip install streamlit-aggrid
 from PIL import Image # pip install pil
+from openpyxl import load_workbook # pip install pil openpyxl
 
 
 # configurações navegador
