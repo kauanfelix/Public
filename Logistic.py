9dd21ff55b6b720f33d00e1fcdfef63a0d382aab
kauanfelix <85039573+kauanfelix@users.noreply.github.com>
2022-12-12 14:49:46 -0300

    Update Logistic.py

--- Logistic.py
+++ Logistic.py
@@ -2,11 +2,10 @@ import pandas as pd
 import streamlit as st
 from st_aggrid import AgGrid, GridOptionsBuilder  # pip install streamlit-aggrid
 from PIL import Image # pip install pil
-from openpyxl import load_workbook # pip install pil openpyxl
+
 
 # configurações navegador
-caminho_icone = 'C:/Users/admin/OneDrive/Nuvem-Kauan/IconeRb.ico'
-caminho_excel = 'C:/Users/admin/OneDrive/Nuvem-Kauan/dados.xlsx'
+caminho_icone = 'IconeRb.ico'
 icone = Image.open(caminho_icone)
 st.set_page_config(page_title="RODOBROTTO SISTEMAS", page_icon=icone, layout="wide")
 aba_simulacao = pd.read_excel(io=caminho_excel, engine='openpyxl', sheet_name='SIMULAR')
